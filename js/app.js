f3e794fb89b939a6b577fe3b567cdb70aecf9a57
ad546 <ad546github@gmail.com>
2018-05-02 09:48:21 -0500

    color change working need to fix appearance

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 $( ".item" ).click(function obtainBox(){
     let box = $( this ).css("background-color");
-    changeColor(box);
+    console.log(box);
+    if (box === "rgb(255, 255, 255)"){
+        $( "#item2" ).css("background-color", "red");
+        $( "#item3" ).css("background-color", "green");
+        $( "#item4" ).css("background-color", "blue");
+    }
+    else {
+        changeColor(box);
+    }
+    
 });
 
 function changeColor(box) {
-    for (let i = 1; i <= 4; i++) {
+    for (let i = 2; i <= 4; i++) {
         $( "#item" + i).css("background-color", box);
     }
 }
