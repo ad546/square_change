b516e9ce72f454e9efb5df77fe6f891ade4dc543
ad546 <ad546github@gmail.com>
2018-05-02 09:52:20 -0500

    cleaned obtainBox and changeColor function

--- js/app.js
+++ js/app.js
@@ -1,20 +1,19 @@
 $( ".item" ).click(function obtainBox(){
     let box = $( this ).css("background-color");
-    console.log(box);
+
+    changeColor(box);
+});
+
+function changeColor(box) {
     if (box === "rgb(255, 255, 255)"){
         $( "#item2" ).css("background-color", "red");
         $( "#item3" ).css("background-color", "green");
         $( "#item4" ).css("background-color", "blue");
     }
     else {
-        changeColor(box);
-    }
-    
-});
-
-function changeColor(box) {
-    for (let i = 2; i <= 4; i++) {
-        $( "#item" + i).css("background-color", box);
+        for (let i = 2; i <= 4; i++) {
+            $( "#item" + i).css("background-color", box);
+        }
     }
 }
 
