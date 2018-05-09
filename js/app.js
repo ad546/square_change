f0c05b16f635188c9199ee279ac8c700f980a60b
ad546 <ad546github@gmail.com>
2018-05-09 09:14:38 -0500

    boxes stay as squares and colors don't have to be reset before changing again

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 $( ".item" ).click(function obtainBox(){
-    let box = $( this ).css("background-color");
+    let box = $( this ).attr('id');
 
     changeColor(box);
 });
 
 function changeColor(box) {
-    if (box === "rgb(255, 255, 255)"){
+    /*if (box === "rgb(255, 255, 255)"){
         $( "#item2" ).css("background-color", "red");
         $( "#item3" ).css("background-color", "green");
         $( "#item4" ).css("background-color", "blue");
@@ -14,6 +14,29 @@ function changeColor(box) {
         for (let i = 2; i <= 4; i++) {
             $( "#item" + i).css("background-color", box);
         }
+    }*/
+    switch(box){
+        case 'item1':
+            $( "#item2" ).css("background-color", "red");
+            $( "#item3" ).css("background-color", "green");
+            $( "#item4" ).css("background-color", "blue");
+            break;
+        case 'item2':
+            $( "#item2" ).css("background-color", "red");
+            $( "#item3" ).css("background-color", "red");
+            $( "#item4" ).css("background-color", "red");
+            break;
+        case 'item3':
+            $( "#item2" ).css("background-color", "green");
+            $( "#item3" ).css("background-color", "green");
+            $( "#item4" ).css("background-color", "green");
+            break;
+        case 'item4':
+            $( "#item2" ).css("background-color", "blue");
+            $( "#item3" ).css("background-color", "blue");
+            $( "#item4" ).css("background-color", "blue");
+            break;
+
     }
 }
 
